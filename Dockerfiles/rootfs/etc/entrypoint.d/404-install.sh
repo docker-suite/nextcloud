c3336b615fc16725bc96d018e9e5ed738a56e9f9
Hexosse <hexosse@gmail.com>
2020-02-05 00:06:39 +0100

    non built-in application must be in the webroot

--- Dockerfiles/rootfs/etc/entrypoint.d/404-install.sh
+++ Dockerfiles/rootfs/etc/entrypoint.d/404-install.sh
@@ -52,10 +52,10 @@ if version_greater "$image_version" "$installed_version"; then
     # use rsync to copy the files from /usr/src/nextcloud to /var/www
     rsync_options="-rlDog --chown ${NEXTCLOUD_USER}:${NEXTCLOUD_GROUP}"
     DEBUG "Syncing $NEXTCLOUD_INSTALL_DIR/ to $NEXTCLOUD_WWW_DIR/"
-    rsync $rsync_options --delete --exclude "/config/" --exclude "/custom_apps/" --exclude "/themes/" "$NEXTCLOUD_INSTALL_DIR/" "$NEXTCLOUD_WWW_DIR/"
+    rsync $rsync_options --delete --exclude "/config/" --exclude "/apps/" --exclude "/appstore/" --exclude "/themes/" "$NEXTCLOUD_INSTALL_DIR/" "$NEXTCLOUD_WWW_DIR/"
 
-    # Update config, appstore and themes folder if they are empty
-    for dir in config themes; do
+    # Update config, apps and themes folder if they are empty
+    for dir in config apps themes; do
         DEBUG "Syncing $NEXTCLOUD_INSTALL_DIR/$dir to $NEXTCLOUD_WWW_DIR/$dir"
         if [ ! -d "${NEXTCLOUD_WWW_DIR}/${dir}" ] || directory_empty "$NEXTCLOUD_WWW_DIR/$dir"; then
             rsync $rsync_options --include /$dir/ --exclude '/*' "$NEXTCLOUD_INSTALL_DIR/" "${NEXTCLOUD_WWW_DIR}/"
@@ -65,7 +65,6 @@ fi
 
 # Move files into volumes (keep original files)
 mvlink "$NEXTCLOUD_WWW_DIR/config" "$NEXTCLOUD_CONFIG_DIR"
-mvlink "$NEXTCLOUD_WWW_DIR/themes" "$NEXTCLOUD_THEMES_DIR"
 
 # Get installed version from config
 # This is necessary if you don't mount /var/www
--- Dockerfiles/rootfs/usr/src/nextcloud/config/apps.config.php
+++ Dockerfiles/rootfs/usr/src/nextcloud/config/apps.config.php
@@ -3,13 +3,13 @@ $CONFIG = array (
     'apps_paths' => array (
       // Read-only location for apps shipped with Nextcloud.
       0 => array (
-        'path'      => getenv('NEXTCLOUD_INSTALL_DIR').'/apps',
+        'path'      => getenv('NEXTCLOUD_WWW_DIR').'/apps',
         'url'       => '/apps',
         'writable'  => false,
       ),
       // Writable location for apps installed from AppStore.
       1 => array (
-        'path'      => getenv('NEXTCLOUD_APPS_DIR'),
+        'path'      => getenv('NEXTCLOUD_WWW_DIR').'/appstore',
         'url'       => '/appstore',
         'writable'  => true,
       ),
